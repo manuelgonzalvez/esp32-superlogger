3c61629384fc040cb047de3d187f3df16ddd8ee8
Manuel Gonzalvez <manuelgonzalvez@gmail.com>
2023-04-14 17:30:42 -0300

    formatting for PIO

---
+++ examples/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include "superLogger.h"
+
+superLogger myDebug("main",superLogger::INFO);
+
+void setup()
+{
+  Serial.begin(460800);
+}
+
+void loop()
+{
+
+
+  // static bool dp;
+
+  // myDebug.printName(dp);
+  // myDebug.printLevel(dp);
+  // myDebug.printColor(dp);
+  // myDebug.printLocation(dp);
+  float pi = PI;
+  log_verbose(myDebug,"Testing  printf formats: Integer: %d Float: %f Char: %c String: %s Hexadecimal: %x Octal: %o Scientific : %e Padding  zeroes: %06d Left-justified: %-15s", 123, 3.14, 'A', "Hello World!", 123, 123, 3.14, 123, "Hello World!");
+  log_info(myDebug,"Testing  printf formats: Integer: %d Float: %f Char: %c String: %s Hexadecimal: %x Octal: %o Scientific : %e Padding  zeroes: %06d Left-justified: %-15s", 123, 3.14, 'A', "Hello World!", 123, 123, 3.14, 123, "Hello World!");
+  log_debug(myDebug,"Testing  printf formats: Integer: %d Float: %f Char: %c String: %s Hexadecimal: %x Octal: %o Scientific : %e Padding  zeroes: %06d Left-justified: %-15s", 123, 3.14, 'A', "Hello World!", 123, 123, 3.14, 123, "Hello World!");
+  log_warning(myDebug,"Testing  printf formats: Integer: %d Float: %f Char: %c String: %s Hexadecimal: %x Octal: %o Scientific : %e Padding  zeroes: %06d Left-justified: %-15s ", 123, 3.14, 'A', "Hello World!", 123, 123, 3.14, 123, "Hello World!");
+  log_error(myDebug,"Testing  printf formats: Integer: %d Float: %f Char: %c String: %s Hexadecimal: %x Octal: %o Scientific : %e Padding  zeroes: %06d Left-justified: %-15s ", 123, 3.14, 'A', "Hello World!", 123, 123, 3.14, 123, "Hello World!");
+  
+  log_error(myDebug,"el valor de pi es" + String(pi)+ " sigo concatenando");
+  log_warning(myDebug,"el valor de pi es" + String(pi)+ " sigo concatenando");
+
+
+  myDebug.setLevel(superLogger::VERBOSE);
+
+  // dp = !dp;
+  delay(1000);
+}
