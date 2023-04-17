bc5157ca65d243a094b8cee74b6f65ad38032a25
Manuel Gonzalvez <manuelgonzalvez@gmail.com>
2023-04-17 11:16:57 -0300

    [MOD] library.json

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,19 +5,19 @@ superLogger myDebug("main",superLogger::INFO);
 
 void setup()
 {
-  Serial.begin(460800);
+  Serial.begin(115200);
 }
 
 void loop()
 {
 
 
-  // static bool dp;
+  static bool dp;
+  myDebug.printName(dp);
+  myDebug.printLevel(dp);
+  myDebug.printColor(dp);
+  myDebug.printLocation(dp);
 
-  // myDebug.printName(dp);
-  // myDebug.printLevel(dp);
-  // myDebug.printColor(dp);
-  // myDebug.printLocation(dp);
   float pi = PI;
   log_verbose(myDebug,"Testing  printf formats: Integer: %d Float: %f Char: %c String: %s Hexadecimal: %x Octal: %o Scientific : %e Padding  zeroes: %06d Left-justified: %-15s", 123, 3.14, 'A', "Hello World!", 123, 123, 3.14, 123, "Hello World!");
   log_info(myDebug,"Testing  printf formats: Integer: %d Float: %f Char: %c String: %s Hexadecimal: %x Octal: %o Scientific : %e Padding  zeroes: %06d Left-justified: %-15s", 123, 3.14, 'A', "Hello World!", 123, 123, 3.14, 123, "Hello World!");
@@ -31,6 +31,6 @@ void loop()
 
   myDebug.setLevel(superLogger::VERBOSE);
 
-  // dp = !dp;
+  dp = !dp;
   delay(1000);
 }
